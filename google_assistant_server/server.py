597a27edb5c11f894a35bbefbc1339a3f157e8ca
Naveen Rawat <rawat.nav@gmail.com>
2017-12-02 01:09:55 +0000

    add logging to file

--- google_assistant_server/server.py
+++ google_assistant_server/server.py
@@ -1,21 +1,28 @@
 #!/usr/bin/env python
+import logging
 from tv_remote import tv_remote
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 
+# initialize log class
+log_file_name='/home/pi/google_home_automation/google_assistant_server/logs.txt'
+logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
+
+# initialize remote class
+tv = tv_remote(logging)
+
 class S(BaseHTTPRequestHandler):
     def do_POST(self):
-        tv = tv_remote()
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
-        print "handling POST request"
-        print post_data
+        logging.debug("handling POST request")
+        logging.debug(post_data)
         tv.process_request(post_data)
 
 def run(server_class=HTTPServer, handler_class=S, port=8080):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
-    print 'Starting httpd on port ' + str(port)
+    logging.debug('Starting httpd on port ' + str(port))
     httpd.serve_forever()
 
 run(port=8080)
--- google_assistant_server/tv_remote.py
+++ google_assistant_server/tv_remote.py
@@ -2,15 +2,19 @@
 from subprocess import call
 
 class tv_remote:
+    def __init__(self, log_class):
+        self.logging = log_class
+        return
+
     def process_request(self, cmd):
-        print "process cmd: " + cmd
+        self.logging.debug("process cmd: " + cmd)
         if cmd == "switch_TV":
-            print "turning on/off TV"
+            self.logging.debug("turning on/off TV")
             cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_POWER"]
             call(cmd_arg)
             return
         if cmd == "volume_up":
-            print "increasing the volume"
+            self.logging.debug("increasing the volume")
             cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_VOLUMEUP"]
             call(cmd_arg)
             call(cmd_arg)
@@ -19,11 +23,29 @@ class tv_remote:
             call(cmd_arg)
             return
         if cmd == "volume_down":
-            print "decreasing the volume"
+            self.logging.debug("decreasing the volume")
             cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_VOLUMEDOWN"]
             call(cmd_arg)
             call(cmd_arg)
             call(cmd_arg)
             call(cmd_arg)
+            call(cmd_arg)
+            return
+        if cmd == "hdmi_1":
+            self.logging.debug("switching to hdmi_1")
+            cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_CYCLEWINDOWS"]
+            call(cmd_arg)
+            cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_LEFT"]
+            call(cmd_arg)
+            cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_OK"]
+            call(cmd_arg)
+            return
+        if cmd == "hdmi_2":
+            self.logging.debug("switching to hdmi_2")
+            cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_CYCLEWINDOWS"]
+            call(cmd_arg)
+            cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_RIGHT"]
+            call(cmd_arg)
+            cmd_arg=["irsend", "SEND_ONCE", "lg_tv_remote", "KEY_OK"]
             call(cmd_arg)
             return
