1768bf86d302060f894b1ac6c4c148155ec2505d
Naveen Rawat <rawat.nav@gmail.com>
2017-12-10 22:58:38 +0000

    fix relative paths & add support for GET command

--- google_assistant_server/server.py
+++ google_assistant_server/server.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
+import os
 import logging
 from tv_remote import tv_remote
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 import SocketServer
 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
 # initialize log class
-log_file_name='/home/pi/google_home_automation/google_assistant_server/logs.txt'
+log_file_name = dir_path + '/logs.txt'
 logging.basicConfig(filename=log_file_name, level=logging.DEBUG)
 
 # initialize remote class
 tv = tv_remote(logging)
 
 class S(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.debug("get request recieved")
+        logging.debug(self.path[1:])
+        self._set_headers()
+        #self.wfile.write("temperature is 201 F")
+        self.wfile.write("<html><body><h1>hi!</h1></body></html>")
+
     def do_POST(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
--- google_assistant_server/tv_remote.py
+++ google_assistant_server/tv_remote.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+import os
 from subprocess import call
+
+fout = open("out.txt", "w")
+ferr = open("err.txt", "w")
+
 
 class tv_remote:
     def __init__(self, log_class):
+        self.dir_path = os.path.dirname(os.path.realpath(__file__))
         self.logging = log_class
         return
 
@@ -10,26 +16,26 @@ class tv_remote:
         self.logging.debug("process cmd: " + cmd)
         if cmd == "switch_TV":
             self.logging.debug("turning on/off TV")
-            cmd_arg=["./power.sh"]
-            call(cmd_arg)
+            fpath = self.dir_path + "/power.sh"
+            call([fpath], stdout=fout, stderr=ferr)
             return
         if cmd == "volume_up":
             self.logging.debug("increasing the volume")
-            cmd_arg=["./volumeup.sh"]
-            call(cmd_arg)
+            fpath = self.dir_path + "/volumeup.sh"
+            call([fpath], stdout=fout, stderr=ferr)
             return
         if cmd == "volume_down":
             self.logging.debug("decreasing the volume")
-            cmd_arg=["./volumedown.sh"]
-            call(cmd_arg)
+            fpath = self.dir_path + "/volumedown.sh"
+            call([fpath], stdout=fout, stderr=ferr)
             return
         if cmd == "hdmi_1":
             self.logging.debug("switching to hdmi_1")
-            cmd_arg=["./hdmi1.sh"]
-            call(cmd_arg)
+            fpath = self.dir_path + "/hdmi1.sh"
+            call([fpath], stdout=fout, stderr=ferr)
             return
         if cmd == "hdmi_2":
             self.logging.debug("switching to hdmi_2")
-            cmd_arg=["./hdmi2.sh"]
-            call(cmd_arg)
+            fpath = self.dir_path + "/hdmi2.sh"
+            call([fpath], stdout=fout, stderr=ferr)
             return
